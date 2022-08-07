4491413cdb197c48076ea4fc5b8c4e74b7fd0207
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-08-06 18:16:20 -0700

    Defining random number.

--- game.py
+++ game.py
@@ -1,7 +1,11 @@
 """A number-guessing game."""
 
+import random
+
 print("Hi")
 
 print(" Welcome to the guessing game!")
 
 name = input("What is your name? ")
+
+pc_number = random.randint(1, 100)
