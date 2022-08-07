464b5432b1148fa033d726c611ff9a68f5a7a70f
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-08-07 13:15:35 -0700

    Ask to play again.

--- game.py
+++ game.py
@@ -4,9 +4,9 @@ import random
 
 
 def guessing_game():
-
+    print()
     print("Hi")
-
+    print()
     name = input("What is your name? ")
 
     print(f"Welcome {name} to the guessing game!")
@@ -23,8 +23,17 @@ def guessing_game():
         if guessed_number == pc_number:
             print(
                 f"Congratulations {name}, you found my number in {tries} tries")
+            print()
+
+            play_again = input("Would you like to play again? Y/N  > ").lower()
+
+            if play_again == "y":
+                guessing_game()
+            else:
+                print("See you next time!")
             break
             print()
+
         elif guessed_number < pc_number:
             print("Your guess was too low, try again.")
             tries = tries + 1
